804c24a383092d082f4f7c33b93454959fc32e29
MatsyshynAnton <anton.matsyshyn@outlook.com>
2021-07-21 18:20:33 +0300

    Add frontend template

---
+++ frontend/ATS/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'ATS';
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:5050/api/Users/3fa85f64-5717-4562-b3fc-2c963f66afa6')
+    .subscribe(_ => console.log(_));
+  }
+}
