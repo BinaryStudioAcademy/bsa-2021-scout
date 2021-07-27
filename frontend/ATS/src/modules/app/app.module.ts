230139a9a619b36ef8d8531ca1b1a30fae2fb45a
R-Mielamud <2m.roman2@gmail.com>
2021-07-27 16:55:29 +0300

    Added frontend structure

--- frontend/ATS/src/modules/app/app.module.ts
+++ frontend/ATS/src/modules/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoutingModule } from '../routing/routing.module';
+import { AppComponent } from './components/app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
