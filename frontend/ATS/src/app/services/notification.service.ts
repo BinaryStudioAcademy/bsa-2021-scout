804257638df5b2ad0b79630d882303ac79668589
Andrew Nadolny <andnad6@gmail.com>
2021-07-27 22:29:46 +0300

    Add a fix for more wrapper flexibility

--- frontend/ATS/src/app/services/notification.service.ts
+++ frontend/ATS/src/app/services/notification.service.ts
@@ -6,35 +6,34 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class NotificationService {
 
-  private _toastrTimeOutInMiliseconds : number = 5000;
-  private _toastrPosition : string = "toast-bottom-right";
-  private _successToastClass : string = "ngx-toastr toast-success toast-success-custom";
-  private _errorToastClass : string = "ngx-toastr toast-error toast-error-custom";
-  private _infoToastClass : string = "ngx-toastr toast-info toast-info-custom";
+  private _toastrTimeOutInMiliseconds: number = 5000;
+  private _toastrPosition: string = "toast-bottom-right";
+  private _successToastClass: string = "ngx-toastr toast-success toast-success-custom";
+  private _errorToastClass: string = "ngx-toastr toast-error toast-error-custom";
+  private _infoToastClass: string = "ngx-toastr toast-info toast-info-custom";
 
-
-  constructor(private _toastr: ToastrService) {
+  constructor(public _toastr: ToastrService) {
     _toastr.toastrConfig.timeOut = this._toastrTimeOutInMiliseconds;
     _toastr.toastrConfig.positionClass = this._toastrPosition;
   }
 
-  public showSuccessMessage(message: string, title: string) {
-    this._toastr.success(message, title, { toastClass : this._successToastClass});
+  public showSuccessMessage(message: string, title: string, options?: {}) {
+    this._toastr.success(message, title, options ?? { toastClass: this._successToastClass });
   }
 
-  public showErrorMessage(message: string, title: string) {
-    this._toastr.error(message, title, { toastClass : this._errorToastClass});
+  public showErrorMessage(message: string, title: string, options?: {}) {
+    this._toastr.error(message, title, options ?? { toastClass: this._errorToastClass });
   }
 
-  public showInfoMessage(message: string, title: string) {
-    this._toastr.info(message, title, { toastClass : this._infoToastClass});
-  }
-  
-  public showDefaultMessage(message: string, title: string) {
-    this._toastr.show(message, title);
+  public showInfoMessage(message: string, title: string, options?: {}) {
+    this._toastr.info(message, title, options ?? { toastClass: this._infoToastClass });
   }
 
-  public showWarningMessage(message: string, title: string) {
-    this._toastr.warning(message, title);
+  public showDefaultMessage(message: string, title: string, options?: {}) {
+    this._toastr.show(message, title, options);
+  }
+
+  public showWarningMessage(message: string, title: string, options?: {}) {
+    this._toastr.warning(message, title, options);
   }
 }
