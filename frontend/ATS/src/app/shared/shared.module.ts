f3261d98be5851ed035b3f04006a244536a2887b
Andrew Nadolny <andnad6@gmail.com>
2021-07-29 14:08:31 +0300

    Add show-password mat-icon

--- frontend/ATS/src/app/shared/shared.module.ts
+++ frontend/ATS/src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { LogoBlockComponent } 
-  from './components/logo-block/logo-block.component';
+import { LogoBlockComponent } from './components/logo-block/logo-block.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
