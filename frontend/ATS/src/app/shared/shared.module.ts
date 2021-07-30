d5efdf2c1f86365467bce95f5bd1d2a8369b07bf
Andrew Nadolny <andnad6@gmail.com>
2021-07-30 09:08:48 +0300

    Chenge location of login compnent and login-block component

--- frontend/ATS/src/app/shared/shared.module.ts
+++ frontend/ATS/src/app/shared/shared.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
-import { LogoBlockComponent } from './components/logo-block/logo-block.component';
-import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    LogoBlockComponent,
-    LoginComponent,
   ],
 })
 export class SharedModule {}
