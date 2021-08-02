a9a1d81cbfa0dd79930cc98038ade868c546ae70
ihedzde <72993239+ihedzde@users.noreply.github.com>
2021-08-02 23:06:19 +0300

    Controller with new ApplicantToTags Endpoints.

--- backend/src/WebAPI/Controllers/ApplicantsController.cs
+++ backend/src/WebAPI/Controllers/ApplicantsController.cs
@@ -3,6 +3,9 @@ using Application.Common.Queries;
 using Application.Applicants.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using System;
+using Application.ApplicantToTags.Dtos;
+using Microsoft.AspNetCore.Http;
 
 namespace WebAPI.Controllers
 {
@@ -23,11 +26,25 @@ namespace WebAPI.Controllers
 
             return Ok(await Mediator.Send(query));
         }
+        [HttpGet("to_tags/{searchRequest}")]
+        public async Task<IActionResult> SearchElasticAsync(string searchRequest)
+        {
+            var query = new GetElasticDocumentsListBySearchRequestQuery<ApplicantToTagsDto>(searchRequest);
 
+            return Ok(await Mediator.Send(query));
+        }
         [HttpPost]
         public async Task<IActionResult> PostApplicantAsync([FromBody] CreateApplicantDto createDto)
         {
             var query = new CreateEntityCommand<CreateApplicantDto>(createDto);
+
+            return Ok(await Mediator.Send(query));
+        }
+        
+        [HttpPost("to_tags/")]
+        public async Task<IActionResult> PostElasticAsync([FromBody] CreateApplicantToTagsDto createDto)
+        {
+            var query = new CreateElasticDocumentCommand<CreateApplicantToTagsDto>(createDto);
 
             return Ok(await Mediator.Send(query));
         }
@@ -39,13 +56,29 @@ namespace WebAPI.Controllers
 
             return Ok(await Mediator.Send(query));
         }
+        [HttpPut("to_tags/")]
+        public async Task<IActionResult> PutElasticAsync([FromBody] UpdateApplicantToTagsDto updateDto)
+        {
+            var query = new UpdateElasticDocumentCommand<UpdateApplicantToTagsDto>(updateDto);
+
+            return Ok(await Mediator.Send(query));
+        }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<IActionResult> DeleteApplicantAsync(string id)
         {
             var query = new DeleteEntityCommand(id);
 
-            return Ok(await Mediator.Send(query));
+            return StatusCode(204, await Mediator.Send(query));
+        }
+
+        [HttpDelete("to_tags/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteElasticAsync(string id)
+        {
+            var query = new DeleteElasticDocumentCommand(id);
+            return StatusCode(204, await Mediator.Send(query));
         }
     }
 }
