055a9a4e3c58f8fe2e146e9ce71244c9ca48618f
SerhiiYanchuk <novograd2201@gmail.com>
2021-08-16 01:47:35 +0300

    hr lead backend

--- backend/src/WebAPI/Controllers/UsersController.cs
+++ backend/src/WebAPI/Controllers/UsersController.cs
@@ -4,10 +4,9 @@ using Application.Users.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using WebAPI.Extensions;
 using Application.Interfaces;
-using Application.Common.Exceptions;
-using Domain.Entities;
+using System.Collections.Generic;
+using Application.Users.Queries;
 
 namespace WebAPI.Controllers
 {
@@ -39,6 +38,14 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> IsEmailAlreadyUsed(string email)
         {
             var query = new IsEntityWithPropertyExistQuery("Email", email);
+            return Ok(await Mediator.Send(query));
+        }
+
+        [Authorize(Roles = "HrLead")]
+        [HttpGet("for-hr-lead")]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetUserForHrLead()
+        {
+            var query = new GetUsersForHrLeadQuery();
             return Ok(await Mediator.Send(query));
         }
 
