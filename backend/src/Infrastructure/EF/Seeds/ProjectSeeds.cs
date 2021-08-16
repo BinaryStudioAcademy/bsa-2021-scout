75fa953ada248424498291db39dee39ea5d300df
ihedzde <72993239+ihedzde@users.noreply.github.com>
2021-08-16 14:15:06 +0300

    Hot fix.

--- backend/src/Infrastructure/EF/Seeds/ProjectSeeds.cs
+++ backend/src/Infrastructure/EF/Seeds/ProjectSeeds.cs
@@ -12,22 +12,27 @@ namespace Infrastructure.EF.Seeds
             new Project{
                 Id = "p9e10160-0522-4c2f-bfcf-a07e9faf0c04",
                 TeamInfo = "Quick Tech",
+                CompanyId = "1"
             },
             new Project{
                 Id = "p8b0e8ca-54ff-4186-8cc0-5f71e1ec1d3c",
                 TeamInfo = "Future Solutions",
+                CompanyId = "1"
             },
             new Project{
                 Id = "p0679037-9b5e-45df-b24d-edc5bbbaaec4",
                 TeamInfo = "Angiko",
+                CompanyId = "1"
             },
             new Project{
                 Id = "paa3320f-866a-4b02-9076-5e8d12796710",
                 TeamInfo = "Fotetifuro",
+                CompanyId = "1"
             },
             new Project{
                 Id = "pd45e3b4-cdf6-4f67-99de-795780c70b8f",
                 TeamInfo = "Merget GmbH",
+                CompanyId = "1"
             }
         };
     }
