257ac7ee668581aa26c12608fce597debd68070d
R-Mielamud <2m.roman2@gmail.com>
2021-08-17 09:56:05 +0300

    Fix

--- backend/src/Application/Users/Commands/RegisterUserCommand.cs
+++ backend/src/Application/Users/Commands/RegisterUserCommand.cs
@@ -59,8 +59,8 @@ namespace Application.Users.Commands
             var sendConfirmEmailMailCommand = new SendConfirmEmailMailCommand(
                 registeredUser,
                 command.RegisterDto.ClientUrl,
-                MailSubjectFactory.confirmEmailMailSubject,
-                MailBodyFactory.confirmEmailMailBody);
+                MailSubjectFactory.CONFIRM_EMAIL,
+                MailBodyFactory.CONFIRM_EMAIL);
             await _mediator.Send(sendConfirmEmailMailCommand);
 
             return Unit.Value;
