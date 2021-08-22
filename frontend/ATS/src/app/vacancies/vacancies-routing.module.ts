53bfffc8e565b0944e9b65263b9a17344f558b2e
R-Mielamud <2m.roman2@gmail.com>
2021-08-22 11:14:44 +0300

    Removed route

--- frontend/ATS/src/app/vacancies/vacancies-routing.module.ts
+++ frontend/ATS/src/app/vacancies/vacancies-routing.module.ts
@@ -1,25 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainContentComponent } from '../shared/components/main-content/main-content.component';
-import { AuthGuard } from '../users/guards/auth.guard';
 
-// This line can't be shorter
-// eslint-disable-next-line max-len
-import { VacanciesStagesBoardComponent } from './components/vacancies-stages-board/vacancies-stages-board.component';
-
-const routes: Routes = [{
-  path: '',
-  component: MainContentComponent,
-  canActivate: [AuthGuard],
-  children: [
-    {
-      path: 'candidates/:id',
-      pathMatch: 'full',
-      component: VacanciesStagesBoardComponent,
-    },
-  ],
-}];
+const routes: Routes = [];
 
 @NgModule({
   exports: [RouterModule],
