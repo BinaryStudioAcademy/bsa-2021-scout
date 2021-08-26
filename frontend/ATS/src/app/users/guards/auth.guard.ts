efa9d19878b3835513633fd14b538cc41fc5179f
SerhiiYanchuk <novograd2201@gmail.com>
2021-08-26 12:56:02 +0300

    oops

--- frontend/ATS/src/app/users/guards/auth.guard.ts
+++ frontend/ATS/src/app/users/guards/auth.guard.ts
@@ -21,7 +21,7 @@ export class AuthGuard implements CanActivateChild, CanActivate {
     if (this.authService.areTokensExist()) {
       return true;
     }
-    this.router.navigate([`${AppRoute.Login}`], { 
+    this.router.navigate([AppRoute.Login], { 
       queryParams: { link: btoa(state.url) },
       queryParamsHandling: state.url.includes('?') ? 'merge' : 'preserve',
     });
