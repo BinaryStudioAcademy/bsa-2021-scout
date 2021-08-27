8856ee2ea64671168043b04903ef563fa8a5ab43
SerhiiYanchuk <novograd2201@gmail.com>
2021-08-27 03:24:17 +0300

    delete extra files fronend

--- frontend/ATS/src/app/vacancies/components/vacancy-card/vacancy-card.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-vacancy-card',
-  templateUrl: './vacancy-card.component.html',
-  styleUrls: ['./vacancy-card.component.scss'],
-})
-export class VacancyCardComponent{
-}
