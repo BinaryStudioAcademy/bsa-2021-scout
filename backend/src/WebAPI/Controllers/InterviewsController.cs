1320bce3f9542a9bb988303e1f8c4f0a57b3f092
ihedzde <72993239+ihedzde@users.noreply.github.com>
2021-09-01 23:21:25 +0300

    Controller for Interviews.

---
+++ backend/src/WebAPI/Controllers/InterviewsController.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interviews.Commands.Create;
+using Application.Interviews.Dtos;
+using Application.Interviews.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    public class InterviewsController : ApiController
+    {
+        public async Task<IActionResult> GetInterviewList()
+        {
+            var query = new GetInterviewsListQuery();
+            return Ok(await Mediator.Send(query));
+        }
+
+        // [HttpGet("{id}")]
+        // public async Task<IActionResult> GetInterview(string id)
+        // {
+        //     var query = new GetInterviewsByIdQuery(id);
+        //     return Ok(await Mediator.Send(query));
+        // }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateInterview(CreateInterviewDto interview)
+        {
+            var command = new CreateInterviewCommand(interview);
+            return StatusCode(201, await Mediator.Send(command));
+        }
+
+        // [HttpPut]
+        // public async Task<IActionResult> UpdateInterview(InterviewDto interview)
+        // {
+        //     var command = new UpdateInterviewCommand(interview);
+        //     return StatusCode(201, await Mediator.Send(command));
+        // }
+
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteInterview(string id)
+        // {
+        //     var command = new DeleteInterviewCommand(id);
+        //     return StatusCode(204, await Mediator.Send(command));
+        // }
+    }
+}
