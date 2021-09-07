37e6a497b5a8ac3f2d33fded51ce1805ecf02974
R-Mielamud <2m.roman2@gmail.com>
2021-09-07 20:11:00 +0300

    Removing query params

--- frontend/ATS/src/app/shared/date-adapters/timezone.date-adapter.ts
+++ frontend/ATS/src/app/shared/date-adapters/timezone.date-adapter.ts
@@ -1,6 +1,10 @@
+import { Injectable } from '@angular/core';
 import { NativeDateAdapter } from '@angular/material/core';
 import { TimezonePipe } from '../pipes/timezone-pipe';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class TimezoneDateAdapter extends NativeDateAdapter {
   public override format(date: Date, _displayFormat: any): string {
     const pipe = new TimezonePipe();
