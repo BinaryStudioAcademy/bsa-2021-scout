b6ab3fefc43c3aa87db7f6a5e6da0df20fabdf3c
SerhiiYanchuk <novograd2201@gmail.com>
2021-09-07 17:43:52 +0300

    add archivation of vacancy, project commands

---
+++ backend/src/Application/Arhive/Dtos/ArchivedProjectDto.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Application.Arhive.Dtos
+{
+    public class ArchivedProjectDto: Dto
+    {
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string TeamInfo { get; set; }
+        public DateTime CreationDate { get; set; }
+        public ArchivedEntityDto ArchivedProjectData { get; set; }
+        public ICollection<ArchivedVacancyShortDto> Vacancies { get; set; } = new List<ArchivedVacancyShortDto>();
+    }
+}
