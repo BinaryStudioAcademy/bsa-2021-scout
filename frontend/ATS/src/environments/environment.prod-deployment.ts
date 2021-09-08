87e56f4465014417188cb2cf55e35c02e5c11e08
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-09-09 01:00:43 +0300

    environment: trying to fix api routing

--- frontend/ATS/src/environments/environment.prod-deployment.ts
+++ frontend/ATS/src/environments/environment.prod-deployment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://prod.bsa21-scout.com/api',
+  apiUrl: 'https://prod.bsa21-scout.com/api/api',
   clientUrl: 'https://prod.bsa21-scout.com',
 };
