d95be9a2ebb93bc20e269558cef219734c82c2c4
R-Mielamud <2m.roman2@gmail.com>
2021-09-10 17:12:32 +0300

    Fixed avatar showing on vacancy stages-candidates board

--- frontend/ATS/src/app/shared/helpers/avatar.ts
+++ frontend/ATS/src/app/shared/helpers/avatar.ts
@@ -2,8 +2,21 @@ import { Applicant } from '../models/applicants/applicant';
 import { OnlyApplicant } from '../models/applicants/only-applicant';
 import { FullVacancyCandidate } from '../models/vacancy-candidates/full';
 
+import { ShortVacancyCandidateWithApplicant }
+  from '../models/vacancy-candidates/short-with-applicant';
+
+const DEFAULT_AVATAR = '../../../../assets/images/defaultAvatar.png';
+
 export function getApplicantAvatar(
-  applicant: FullVacancyCandidate | OnlyApplicant | Applicant,
+  applicant:
+  | FullVacancyCandidate 
+  | ShortVacancyCandidateWithApplicant
+  | OnlyApplicant
+  | Applicant,
 ): string {
-  return applicant.photoLink ?? '../../../../assets/images/defaultAvatar.png';
+  if (applicant.hasOwnProperty('photoLink')) {
+    return (applicant as any).photoLink ?? DEFAULT_AVATAR;
+  }
+
+  return (applicant as any).applicant.photoLink ?? DEFAULT_AVATAR;
 }
